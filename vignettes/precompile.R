7bd0d08ffdb7db7cd308f6437fcf41b90fd89100
Joshua W. Lambert <joshua.lambert@lshtm.ac.uk>
2025-01-24 06:50:02 +0000

    version 0.4.1

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,9 +1,20 @@
 # Pre-compiled vignettes that take a long time to render
-# Must manually move image files from epiparameter/ to epiparameter/vignettes/
-# after knit
 knitr::knit(
   # _ prefix stops pkgdown from re-rendering the original
   # https://pkgdown.r-lib.org/reference/build_articles.html
-  input = file.path("vignettes", "_extract-bias.Rmd.orig"),
-  output = file.path("vignettes", "extract-bias.Rmd")
+  input = file.path("vignettes", "articles", "_extract-bias.Rmd.orig"),
+  output = file.path("vignettes", "articles", "extract-bias.Rmd")
 )
+
+# run from package root directory
+figures <- file.path(list.files(path = ".", pattern = "^plot-.*\\.png$"))
+cp <- file.copy(
+  from = file.path(".", figures),
+  to = file.path("vignettes", "articles"),
+  overwrite = TRUE
+)
+if (all(cp)) {
+  unlink(figures)
+} else {
+  print("Article figures did not copy, please inspect manually.")
+}
